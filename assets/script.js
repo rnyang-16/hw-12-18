8e252a89313b1d3855843d8f8d6510e5964158ff
Ruonan Y <joyceyang0730@gmail.com>
2019-12-17 13:34:21 -0800

    Generally completed, charset needed to be fixed

---
+++ assets/script.js
@@ -0,0 +1,93 @@
+function promptPasswordLength() {
+    while (true) {
+      inp_len = prompt("Please enter the password length (must bewtten 8 and 128 characters).")
+      if (inp_len == null) { // user cancels
+        return null;
+      }
+
+      else {
+        var n = parseInt(inp_len);
+        if (n >= 8 && n <= 128){
+          return n;
+        }
+      }                       
+    }
+}
+
+var CHARACTER_SETS = [
+    [true, "Numbers", "0123456789"],
+    [true, "Lowercase", "abcdefghijklmnopqrstuvwxyz"],
+    [false, "Uppercase", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+    [false, "ASCII symbols", "!\"#$%" + String.fromCharCode(38) + "'()*+,-./:;" + String.fromCharCode(60) + "=>?@[\\]^_`{|}~"],
+    [false, "Space", " "],
+];
+
+function promptCharTypes() {
+    var useSpecialChar = confirm("Special characters?");
+    var useNumChar = confirm("Numeric characters?");
+    var useLowerChar = confirm("Lowercase characters?");
+    var useUpperChar = confirm("Uppercase characters?");
+
+    return {
+        'useSpecialChar': useSpecialChar,
+        'useNumChar': useNumChar,
+        'useLowerChar': useLowerChar,
+        'useUpperChar': useUpperChar,
+    };
+}
+
+function randomChoice (arr) {
+    return arr[Math.floor(Math.random()*arr.length)];
+}  
+
+function getRandomPassword(pw_len, char_types) {
+    var charset = '';
+    if (char_types['useSpecialChar']){
+    charset = charset.concat(CHARACTER_SETS[3,2]);
+    }
+    if (char_types['useNumChar']){
+    charset = charset.concat(CHARACTER_SETS[0,2]);
+    }
+    if (char_types['useLowerChar']){
+    charset = charset.concat(CHARACTER_SETS[1,2]);
+    }
+    if (char_types['useUpperChar']){
+    charset = charset.concat(CHARACTER_SETS[2,2]);
+    }
+    charset = charset.split('')
+    console.log(charset);
+
+    password = ''
+
+    var i;
+    for (i = 0; i < inp_len.length; i += 1){
+    password = password.concat(randomChoice(charset));
+    }
+
+    document.getElementById("password").setAttribute("value", password);
+}
+
+
+function passwordGenerator() {
+    var inp_len = promptPasswordLength();
+    if (inp_len != null){
+    char_types = promptCharTypes();
+    }
+    if (char_types != null){
+        getRandomPassword(inp_len, char_types);
+    }
+    return null;       
+}
+
+function copyToClipboard(){
+    var copyText = document.getElementById("password");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); 
+    document.execCommand("copy");
+    if (copyText.value.length == 0) {
+    alert("Please generate a password first");
+    }
+    else {
+    alert("Copied the text: " + copyText.value);
+    }
+}
